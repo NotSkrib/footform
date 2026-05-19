a385845e6493f3e5f9259b6552db317e909674f6
NotSkrib <leonardofabbrini11@gmail.com>
2026-05-19 21:47:05 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
         fontSrc: ["'self'", "https://fonts.gstatic.com"],
         imgSrc: ["'self'", "data:"],
-        scriptSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
       },
     },
   })
